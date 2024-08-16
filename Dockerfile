284c0babfcd34c0bd2cfc1801af8522095e46d6f
rskliar <rskliar@tango.me>
2024-08-16 13:43:02 +0300

    Login page added

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /app
 
 RUN pip install poetry
 
-COPY ./decks_tool /app
+COPY ./decks_tool /app/decks_tool
 COPY pyproject.toml poetry.lock /app/
 
 RUN poetry install --no-dev
@@ -13,4 +13,4 @@ EXPOSE 8501
 
 HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
 
-ENTRYPOINT ["poetry", "run", "streamlit", "run", "decks_tool.py", "--server.port=8501", "--server.address=0.0.0.0"]
+ENTRYPOINT ["poetry", "run", "streamlit", "run", "decks_tool/decks_tool.py", "--server.port=8501", "--server.address=0.0.0.0"]
--- decks_tool/decks_tool.py
+++ decks_tool/decks_tool.py
@@ -1,40 +1,30 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 
-"""
-# Welcome to Streamlit!
+st.set_page_config(initial_sidebar_state="collapsed")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+with open('decks_tool/config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+authenticator.login()
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+if st.session_state['authentication_status']:
+    authenticator.logout(location='sidebar', key='main_logout')
+    st.markdown(
+        "<h3 style='color: green;'>Welcome to RIDDLY decks management tool</h3>",
+        unsafe_allow_html=True
+    )
+else:
+    if st.session_state['authentication_status'] is False:
+        st.error('Username/password is incorrect')
+    elif st.session_state['authentication_status'] is None:
+        st.warning('Please enter your username and password')
